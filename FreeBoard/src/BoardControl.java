6c9709cbf71bfa3b18138d38364bd53aa6de1794
Park BeomSu <qjatn8595@gmail.com>
2021-01-26 15:52:57 +0900

    자바 게시판 로그인 기능 구현중, 훼손파일 복구

---
+++ FreeBoard/src/BoardControl.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BoardControl {
+    public BoardControl() {
+    }
+
+    public void Write() {
+        try{
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+            String line = "";
+            while(line.equals("EOF") != true ){
+                line = br.readLine();
+                System.out.println(line);
+            }
+            br.close();
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ FreeBoard/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        String user_id;
+        String user_password;
+        System.out.println("=== 로그인 화면 ===");
+        Scanner sc = new Scanner(System.in);
+        user_id = sc.nextLine();
+        user_password = sc.nextLine();
+
+        membership m1 = new membership(user_id, user_password);
+        m1.Signup();
+
+
+    }
+}
